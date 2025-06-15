6c0d0c66a914997794c1b1c5226b941a3865a2c9
m01310g <m01310g@gmail.com>
2025-06-15 22:03:04 +0900

    [chore] 06/15 공통 색상 설정

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
+  },
+  devIndicators: false,
+  webpack(config) {
+    config.resolve.alias["tailwindConfig$"] = path.resolve(
+      __dirname,
+      "tailwind.config.js",
+    );
+    return config;
   },
 };
 
