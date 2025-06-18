99b956cdcfb3170107970b2b0849cea1161ac79d
m01310g <m01310g@gmail.com>
2025-06-19 00:11:44 +0900

    [feat] 06/19 폼 제출 방지 코드 추가 #4

--- app/profile/create/components/AddButton.tsx
+++ app/profile/create/components/AddButton.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 const AddButton = () => {
   return (
-    <button className="border-border-secondary text-body-2 flex items-center gap-0.5 rounded-[8px] border px-3 py-2 text-neutral-400 hover:bg-neutral-100">
+    <button
+      className="border-border-secondary text-body-2 flex items-center gap-0.5 rounded-[8px] border px-3 py-2 text-neutral-400 hover:bg-neutral-100"
+      onClick={(e) => e.preventDefault()}
+    >
       <Image src={"/icons/plus.svg"} width={16} height={16} alt="추가 버튼" />
       추가
     </button>
