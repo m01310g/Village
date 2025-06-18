e676631dad27b454077fa6169f66ecf8503695f8
m01310g <m01310g@gmail.com>
2025-06-19 00:16:16 +0900

    [feat] 06/19 pathname 따라 헤더 타이틀 동적 변경 #4

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Header from "../components/Header";
 
 const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
+  const getTitleByPath = (path: string) => {
+    if (path === "/profile") return "빌리지";
+    if (path === "/profile/create") return "프로필 등록";
+  };
+
   return (
     <>
-      <Header title="프로필" showBackButton />
+      <Header title={getTitleByPath(pathname) || "프로필"} showBackButton />
       <main className="h-[calc(100vh-46px)] overflow-y-auto">{children}</main>
     </>
   );
