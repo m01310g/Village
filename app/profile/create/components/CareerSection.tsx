54d01350cb02d6d520f537ceb6049a0d4a7e8368
m01310g <m01310g@gmail.com>
2025-06-20 00:22:22 +0900

    [feat] 06/20 프로필 등록 컴포넌트 분리 #4

---
+++ app/profile/create/components/CareerSection.tsx
@@ -0,0 +1,21 @@
+import AddButton from "./AddButton";
+import ProfileLabel from "./ProfileLabel";
+
+const CareerSection = () => {
+  return (
+    <section className="flex flex-col gap-3">
+      <div className="flex items-center justify-between">
+        <ProfileLabel label="경력사항" bold />
+        <AddButton />
+      </div>
+      <div className="h-[1px] w-full bg-border-secondary" />
+      <p className="text-body-2 py-3 text-center text-neutral-400">
+        경력사항을 자유롭게 작성해주세요!
+        <br />
+        신입이면 넘어가셔도 좋습니다.
+      </p>
+    </section>
+  );
+};
+
+export default CareerSection;
