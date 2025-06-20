dba2f208e3c1dfb46c02d3514ae12790e21e75d6
m01310g <m01310g@gmail.com>
2025-06-20 15:50:41 +0900

    [feat] 06/20 경력사항 추가 바텀시트 구현 #4

--- app/profile/create/components/career/CareerAddButton.tsx
+++ app/profile/create/components/career/CareerAddButton.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
+import { useState } from "react";
+import CareerAddBottomSheet from "./CareerAddBottomSheet";
 
 const CareerAddButton = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleAddButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setOpen(true);
+  };
+
   return (
-    <button
-      className="text-body-2 flex items-center gap-0.5 rounded-[8px] border border-border-secondary px-3 py-2 text-neutral-400 hover:bg-neutral-100"
-      onClick={(e) => e.preventDefault()}
-    >
-      <Image src={"/icons/plus.svg"} width={16} height={16} alt="추가 버튼" />
-      추가
-    </button>
+    <>
+      <button
+        className="text-body-2 flex items-center gap-0.5 rounded-[8px] border border-border-secondary px-3 py-2 text-neutral-400 hover:bg-neutral-100"
+        onClick={handleAddButtonClick}
+      >
+        <Image src={"/icons/plus.svg"} width={16} height={16} alt="추가 버튼" />
+        추가
+      </button>
+      {open && <CareerAddBottomSheet />}
+    </>
   );
 };
 export default CareerAddButton;
