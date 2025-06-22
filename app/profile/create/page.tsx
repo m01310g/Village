9ead757fce81a7046772d57f3b1e87aafae312c7
m01310g <m01310g@gmail.com>
2025-06-23 00:14:33 +0900

    [feat] 06/23 프로필 등록 api 연동 #16

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -5,6 +5,9 @@ import { useInputValidation } from "../hooks/useInputValidation";
 import { useState } from "react";
 import ProfileForm from "../components/ProfileForm";
 import { WebCareer } from "../types/webCareer";
+import { useAuthStore } from "@/store/useAuthStore";
+import { ErrorResponse } from "@/app/types/ErrorResponse";
+import { useRouter } from "next/navigation";
 
 const ProfileCreatePage = () => {
   const nameInput = useInputValidation("name");
@@ -17,6 +20,8 @@ const ProfileCreatePage = () => {
     webCareers: [],
     introduction: "",
   });
+  const accessToken = useAuthStore((state) => state.accessToken);
+  const router = useRouter();
 
   const isFormValid =
     !!nameInput.value &&
@@ -33,8 +38,44 @@ const ProfileCreatePage = () => {
     setFormData((prev) => ({ ...prev, [field]: value }));
   };
 
-  const handleSubmit = () => {
-    console.log(formData);
+  const handleSubmit = async () => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/web-profile/registerWebProfile`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          body: JSON.stringify(formData),
+        },
+      );
+
+      if (!res.ok) {
+        const error: ErrorResponse = await res.json();
+        if (error.statusCode === 400) {
+          throw new Error(`요청 형식 오류: ${error.message}`);
+        } else if (error.statusCode === 401) {
+          throw new Error(
+            `유효하지 않거나 기간이 만료된 토큰: ${error.message}`,
+          );
+        } else if (error.statusCode === 403) {
+          throw new Error(`유저 회원이 아닙니다: ${error.message}`);
+        } else if (error.statusCode === 409) {
+          throw new Error(`이미 등록된 프로필이 있습니다: ${error.message}`);
+        } else {
+          throw new Error(error.message);
+        }
+      }
+
+      const result = await res.json();
+      const userId = result.data.id;
+
+      router.replace(`/profile/${userId}`);
+    } catch (err: any) {
+      console.error(err instanceof Error ? err.message : "알 수 없는 오류");
+    }
   };
 
   return (
