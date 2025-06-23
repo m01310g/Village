3185e2e4a646e6db4634d9a5e11ce70c3c87dd38
m01310g <m01310g@gmail.com>
2025-06-24 03:14:09 +0900

    [feat] 06/24 댓글 작성 창 색상 변경 구현 #18

--- app/post/[postId]/components/comments/CommentCreateSection.tsx
+++ app/post/[postId]/components/comments/CommentCreateSection.tsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import CommentCreateButton from "./CommentCreateButton";
+import clsx from "clsx";
 
 const CommentCreateSection = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <section className="fixed bottom-0 flex w-full max-w-[375px] items-center justify-center gap-1 bg-background-primary px-4 py-[11px]">
       <input
         type="text"
         placeholder="댓글을 입력해주세요."
-        className="placeholder:text-body-3 w-full rounded-[26px] bg-background-secondary px-4 py-3 placeholder:text-text-tertiary focus:outline-none"
+        className={clsx(
+          "placeholder:text-body-3 w-full rounded-[26px] px-4 py-3 placeholder:text-text-tertiary focus:outline-none",
+          inputValue
+            ? "border border-border-secondary bg-background-primary text-text-primary"
+            : "bg-background-secondary",
+        )}
+        onChange={handleChange}
+        value={inputValue}
       />
-      <CommentCreateButton />
+      <CommentCreateButton isActive={!!inputValue} />
     </section>
   );
 };
