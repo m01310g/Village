b9936b4e9ad94b7b30a2c968c859106e5a063b2b
m01310g <m01310g@gmail.com>
2025-06-26 01:21:43 +0900

    [feat] 06/26 로그인 안 된 사용자 alert #22

--- app/post/[postId]/components/PostDetailFooter.tsx
+++ app/post/[postId]/components/PostDetailFooter.tsx
@@ -3,6 +3,9 @@ import clsx from "clsx";
 import { useState } from "react";
 import { useLikePost } from "../hooks/useLikePost";
 import { useUnlikePost } from "../hooks/useUnlikePost";
+import { useAuthStore } from "@/store/useAuthStore";
+import ModalWrapper from "@/app/components/modal/ModalWrapper";
+import Button from "@/app/components/Button";
 
 interface PostDetailFooterProps {
   postId: number;
@@ -21,8 +24,14 @@ const PostDetailFooter = ({
   const [count, setCount] = useState(likeCount);
   const likeMutation = useLikePost(postId);
   const unlikeMutation = useUnlikePost(postId);
+  const accessToken = useAuthStore.getState().accessToken;
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleLike = () => {
+    if (!accessToken) {
+      setIsModalOpen(true);
+      return;
+    }
     setLiked((prev) => !prev);
     setCount((prev) => (liked ? prev - 1 : prev + 1));
     if (liked) {
@@ -33,26 +42,36 @@ const PostDetailFooter = ({
   };
 
   return (
-    <div className="flex items-center justify-between">
-      <button
-        className={clsx(
-          "text-caption-3 flex cursor-pointer items-center justify-center gap-1 rounded-[99px] border px-2 py-[6px]",
-          liked
-            ? "border-border-brand bg-background-brandSecondary text-text-onsecondary"
-            : "border-border-secondary bg-background-primary text-text-tertiary",
-        )}
-        onClick={handleLike}
-      >
-        <HeartIcon
-          width="14px"
-          height="14px"
-          color={liked ? "#00a6f4" : "#737373"}
-        />
-        <span className="leading-none">좋아요</span>
-        <span className="leading-none">{count}</span>
-      </button>
-      <span className="text-caption-3 text-text-tertiary">{createdAt}</span>
-    </div>
+    <>
+      <div className="flex items-center justify-between">
+        <button
+          className={clsx(
+            "text-caption-3 flex cursor-pointer items-center justify-center gap-1 rounded-[99px] border px-2 py-[6px]",
+            liked
+              ? "border-border-brand bg-background-brandSecondary text-text-onsecondary"
+              : "border-border-secondary bg-background-primary text-text-tertiary",
+          )}
+          onClick={handleLike}
+        >
+          <HeartIcon
+            width="14px"
+            height="14px"
+            color={liked ? "#00a6f4" : "#737373"}
+          />
+          <span className="leading-none">좋아요</span>
+          <span className="leading-none">{count}</span>
+        </button>
+        <span className="text-caption-3 text-text-tertiary">{createdAt}</span>
+      </div>
+      {isModalOpen && (
+        <ModalWrapper onClose={() => setIsModalOpen(false)}>
+          <h3 className="text-title-3 text-text-primary">
+            로그인이 필요한 서비스입니다.
+          </h3>
+          <Button onClick={() => setIsModalOpen(false)}>확인</Button>
+        </ModalWrapper>
+      )}
+    </>
   );
 };
 
