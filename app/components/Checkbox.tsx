8136c23352a16e6f4b11c702e1d67f5fbe343710
m01310g <m01310g@gmail.com>
2025-06-27 21:23:21 +0900

    [feat] 06/27 전화번호 공개 여부 UI 구현 #28

--- app/components/Checkbox.tsx
+++ app/components/Checkbox.tsx
@@ -3,16 +3,17 @@ import CheckIcon from "@/public/icons/check.svg";
 
 interface CheckboxProps {
   isChecked: boolean;
+  onClick: () => void;
 }
 
-const Checkbox = ({ isChecked }: CheckboxProps) => {
+const Checkbox = ({ isChecked, onClick }: CheckboxProps) => {
   return (
-    <div className="h-5 w-5 p-0.5">
+    <div className="h-5 w-5 cursor-pointer p-0.5" onClick={onClick}>
       <div
         className={clsx(
           isChecked
-            ? "border border-blue-600 bg-blue-600"
-            : "border border-border-secondary bg-background-primary",
+            ? "rounded-[3px] border border-blue-600 bg-blue-600"
+            : "rounded-[3px] border-[1.5px] border-border-secondary bg-background-primary",
         )}
       >
         <CheckIcon width="14px" height="14px" color="#ffffff" />
--- app/profile/create/detail/components/PhoneNumberVisibilitySeciton.tsx
+++ app/profile/create/detail/components/PhoneNumberVisibilitySeciton.tsx
@@ -1,9 +1,26 @@
+import Checkbox from "@/app/components/Checkbox";
 import ProfileLabel from "../../components/ProfileLabel";
+import { useState } from "react";
 
 const PhoneNumberVisibilitySection = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
     <section className="flex flex-col gap-3">
       <ProfileLabel label="전화번호 공개 여부" bold />
+      <div className="flex h-[31px] w-full items-center gap-2">
+        <Checkbox
+          isChecked={isChecked}
+          onClick={() => setIsChecked((prev) => !prev)}
+        />
+        <span className="text-caption-2 text-text-primary">
+          센터에게 내 연락처를 보여줘도 괜찮아요.
+        </span>
+      </div>
+      <span className="text-caption-3 text-text-info">
+        연락처를 비공개하면 센터의 제안을 받을 수 없어요. 제안을 받으려면
+        연락처를 공개해 주세요. (언제든 비공개 전환 가능)
+      </span>
     </section>
   );
 };
