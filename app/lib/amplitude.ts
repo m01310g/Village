f905b5aac1f4248704ceb7b40828f41fda625941
m01310g <m01310g@gmail.com>
2025-06-29 22:39:20 +0900

    [feat] 06/29 앰플리튜드 설정 변경

--- app/lib/amplitude.ts
+++ app/lib/amplitude.ts
@@ -1,11 +1,11 @@
 import * as amplitude from "@amplitude/analytics-browser";
 
 export const initAmplitude = () => {
-  amplitude.init(`${process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY}`);
+  amplitude.init(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY!);
 };
 
 export const logSignUpEvent = (userId: number) => {
-  amplitude.setUserId(userId.toString());
+  amplitude.setUserId(`web-user-${userId}`);
   amplitude.track("register_profile", {
     timestamp: new Date().toISOString(),
   });
