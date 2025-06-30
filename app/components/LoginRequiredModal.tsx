8ded0290cb57a9d02040e8dbb3e7c08bf4a55b91
m01310g <m01310g@gmail.com>
2025-07-01 02:06:36 +0900

    [feat] 07/01 모달 확인 클릭 시 프로필 화면으로 라우팅 #42

--- app/components/LoginRequiredModal.tsx
+++ app/components/LoginRequiredModal.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/navigation";
 import Button from "./Button";
 import ModalWrapper from "./modal/ModalWrapper";
 
@@ -6,12 +7,21 @@ interface LoginRequiredModalProps {
 }
 
 const LoginRequiredModal = ({ setIsModalOpen }: LoginRequiredModalProps) => {
+  const router = useRouter();
+
   return (
     <ModalWrapper onClose={() => setIsModalOpen(false)}>
       <h3 className="text-title-3 text-text-primary">
         로그인이 필요한 서비스입니다.
       </h3>
-      <Button onClick={() => setIsModalOpen(false)}>확인</Button>
+      <Button
+        onClick={() => {
+          setIsModalOpen(false);
+          router.push("/profile");
+        }}
+      >
+        확인
+      </Button>
     </ModalWrapper>
   );
 };
