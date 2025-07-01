ce616d7998821e53d856434329203c6ac4cd7080
m01310g <m01310g@gmail.com>
2025-07-02 02:15:41 +0900

    [fix] 07/02 게시글 유형 수정 시 수정 버튼 활성화 #56

--- app/post/[postId]/edit/page.tsx
+++ app/post/[postId]/edit/page.tsx
@@ -5,9 +5,9 @@ import { useCallback, useEffect, useState } from "react";
 import { usePostData } from "../hooks/usePostData";
 import { useParams, useRouter } from "next/navigation";
 import PostForm from "../components/PostForm";
-import { fetchWithAuth } from "@/app/lib/api/fetchWithAuth";
 import ModalWrapper from "@/app/components/modal/ModalWrapper";
 import Button from "@/app/components/Button";
+import { useEditPost } from "../hooks/useEditPost";
 
 const PostEditPage = () => {
   const setHeader = useSetHeader();
@@ -16,6 +16,9 @@ const PostEditPage = () => {
   const [isActive, setIsActive] = useState<"업계이야기" | "채용" | "교육">(
     "업계이야기",
   );
+  const [originalIsActive, setOriginalIsActive] = useState<
+    "업계이야기" | "채용" | "교육"
+  >("업계이야기");
   const [content, setContent] = useState("");
   const [originalContent, setOriginalContent] = useState("");
   const [images, setImages] = useState<string[]>([]);
@@ -25,59 +28,23 @@ const PostEditPage = () => {
 
   const { data: postData, isLoading } = usePostData(postId);
 
+  const typeMap = { 업계이야기: 0, 채용: 1, 교육: 2 } as const;
+
+  const formData = {
+    id: postData!.id,
+    type: typeMap[isActive],
+    content,
+    images,
+  };
+  const editPostMutation = useEditPost({ ...formData });
+
   const arraysAreEqual = (a: string[], b: string[]) => {
     if (a.length !== b.length) return false;
     return a.every((val, idx) => val === b[idx]);
   };
 
   const handleEdit = useCallback(async () => {
-    const typeMap = { 업계이야기: 0, 채용: 1, 교육: 2 } as const;
-
-    const formData = {
-      id: postData!.id,
-      type: typeMap[isActive],
-      content,
-      images,
-    };
-
-    try {
-      const res = await fetchWithAuth(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/web-community/modifyBoard`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-          method: "POST",
-          body: JSON.stringify(formData),
-        },
-      );
-
-      if (!res.ok) {
-        const error = await res.json();
-        if (error.statusCode === 400) {
-          throw new Error(`요청 형식 오류: ${error.message}`);
-        } else if (error.statusCode === 401) {
-          throw new Error(
-            `유효하지 않거나 기간이 만료된 토큰: ${error.message}`,
-          );
-        } else if (error.statusCode === 403) {
-          throw new Error(`유저 회원이 아닙니다: ${error.message}`);
-        } else if (error.statusCode === 404) {
-          throw new Error(`등록된 프로필 없음: ${error.message}`);
-        } else {
-          throw new Error(error.message);
-        }
-      }
-
-      const result = await res.json();
-      const data = result.data;
-
-      router.replace(`/post/${data.id}`);
-    } catch (err) {
-      console.error(
-        err instanceof Error ? `게시글 등록 실패: ${err.message}` : err,
-      );
-    }
+    editPostMutation.mutate();
   }, [router, isActive, content, images, postData]);
 
   useEffect(() => {
@@ -91,7 +58,10 @@ const PostEditPage = () => {
       const types = ["업계이야기", "채용", "교육"];
 
       if (typeof postData.type === "number") {
-        setIsActive(types[postData.type] as "업계이야기" | "채용" | "교육");
+        const category = types[postData.type] as "업계이야기" | "채용" | "교육";
+
+        setIsActive(category);
+        setOriginalIsActive(category);
       }
       setContent(postData.content);
       setImages(postData.images || []);
@@ -103,7 +73,8 @@ const PostEditPage = () => {
   useEffect(() => {
     const isContentChanged = content !== originalContent;
     const isImagesChanged = !arraysAreEqual(images, originalImages);
-    const isChanged = isContentChanged || isImagesChanged;
+    const isTypeChanged = isActive !== originalIsActive;
+    const isChanged = isContentChanged || isImagesChanged || isTypeChanged;
 
     const editButtonProps = {
       className: isChanged ? "text-text-onsecondary" : "text-text-disabled",
@@ -140,7 +111,7 @@ const PostEditPage = () => {
           <Button
             color="primary"
             size="md"
-            onClick={() => router.push(`/post/${postData.id}`)}
+            onClick={() => router.replace(`/post/${postData.id}`)}
           >
             확인
           </Button>
---
+++ app/post/[postId]/hooks/useEditPost.ts
@@ -0,0 +1,65 @@
+import { fetchWithAuth } from "@/app/lib/api/fetchWithAuth";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+
+interface EditPostType {
+  id: number;
+  type: 0 | 1 | 2;
+  content: string;
+  images: string[];
+}
+
+const editPost = async (formData: EditPostType) => {
+  try {
+    const res = await fetchWithAuth(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/web-community/modifyBoard`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(formData),
+      },
+    );
+
+    if (!res.ok) {
+      const error = await res.json();
+      if (error.statusCode === 400) {
+        throw new Error(`요청 형식 오류: ${error.message}`);
+      } else if (error.statusCode === 401) {
+        throw new Error(`유효하지 않거나 기간이 만료된 토큰: ${error.message}`);
+      } else if (error.statusCode === 403) {
+        throw new Error(`유저 회원이 아닙니다: ${error.message}`);
+      } else if (error.statusCode === 404) {
+        throw new Error(`등록된 프로필 없음: ${error.message}`);
+      } else {
+        throw new Error(error.message);
+      }
+    }
+
+    const result = await res.json();
+    const data = result.data;
+    return data;
+  } catch (err) {
+    throw new Error(
+      err instanceof Error
+        ? `게시글 등록 실패: ${err.message}`
+        : `게시글 등록 실패: ${String(err)}`,
+    );
+  }
+};
+
+export const useEditPost = (formData: EditPostType) => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  if (!formData) return { mutate: () => {} };
+
+  return useMutation({
+    mutationFn: () => editPost(formData),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["postData", data.id] });
+      router.replace(`/post/${data.id}`);
+    },
+  });
+};
