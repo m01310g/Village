3c161fa95d19d5e16bacd3243f9df1c4c6b8f147
m01310g <m01310g@gmail.com>
2025-07-02 01:12:41 +0900

    [feat] 07/02 채용 공고 item 구현 #53

---
+++ app/recruit/components/RecruitItem.tsx
@@ -0,0 +1,42 @@
+interface RecruitItemProps {
+  centerName: string;
+  location: string;
+  workFormat: string[];
+  workTime: string[];
+}
+
+const RecruitItem = ({
+  centerName,
+  location,
+  workFormat,
+  workTime,
+}: RecruitItemProps) => {
+  return (
+    <>
+      <div className="flex cursor-pointer flex-col gap-2 px-4 py-3">
+        <h2 className="text-title-3 text-neutral-900">{centerName}</h2>
+        <div className="flex flex-col gap-1">
+          <span className="text-caption-3 text-text-tertiary">{location}</span>
+          {workFormat.map((f, i) => (
+            <span className="text-caption-3 text-text-tertiary" key={i}>
+              {f}
+            </span>
+          ))}
+          <div className="flex gap-1">
+            {workTime.map((t, i) => (
+              <span
+                key={i}
+                className="text-caption-3 rounded-[4px] bg-neutral-50 p-1 text-text-tertiary"
+              >
+                {t}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="h-0.5 bg-border-primary" />
+    </>
+  );
+};
+
+export default RecruitItem;
--- app/recruit/page.tsx
+++ app/recruit/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useSetHeader } from "../components/header/HeaderContext";
 import RecruitSearchBar from "./components/RecruitSearchBar";
 import RegionFilterSettingBar from "./components/RegionFilterSettingBar";
+import RecruitItem from "./components/RecruitItem";
 
 const RecruitPage = () => {
   const setHeader = useSetHeader();
@@ -16,8 +17,8 @@ const RecruitPage = () => {
   }, [setHeader]);
 
   return (
-    <main className="text-title-1 flex h-[calc(100vh-46px-81px)] flex-col gap-3 bg-background-primary text-text-primary">
-      <div className="flex flex-col gap-3 p-4">
+    <main className="text-title-1 flex h-[calc(100vh-46px-81px)] flex-col gap-3 bg-background-primary py-4 text-text-primary">
+      <div className="flex flex-col gap-3 px-4">
         <RecruitSearchBar keyword={keyword} setKeyword={setKeyword} />
         <h3 className="text-title-3 text-neutral-900">{recruitCount}건</h3>
       </div>
@@ -25,6 +26,12 @@ const RecruitPage = () => {
         <RegionFilterSettingBar
           selectedDistricts={selectedDistricts}
           setSelectedDistricts={setSelectedDistricts}
+        />
+        <RecruitItem
+          centerName="에이블짐 서초점"
+          location="서울시 서초구"
+          workFormat={["계약직"]}
+          workTime={["자유근무", "오전 파트타임"]}
         />
       </div>
     </main>
