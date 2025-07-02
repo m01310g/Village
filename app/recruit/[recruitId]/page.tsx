733cbdababd142ea97666883d5aa9213d37789c8
m01310g <m01310g@gmail.com>
2025-07-03 01:07:39 +0900

    [feat] 07/03 근무조건 탭 구현 #53

--- app/recruit/[recruitId]/page.tsx
+++ app/recruit/[recruitId]/page.tsx
@@ -1,14 +1,23 @@
 "use client";
 
+import { useState } from "react";
 import CenterNameSection from "./components/CenterNameSection";
+import TabTitle from "./components/TabTitle";
 
 const RecruitByIdPage = () => {
+  const tabs = [
+    { key: "condition", label: "근무조건" },
+    { key: "", label: "" },
+  ];
+  const [activeTab, setActiveTab] = useState("condition");
+
   return (
     <main className="h-[calc(100vh-46px-81px)] bg-background-primary">
       <CenterNameSection
         centerName="00휘트니스 서현점"
         createdAt="2025.11.03"
       />
+      <TabTitle tabs={tabs} activeTab={activeTab} onTabChange={setActiveTab} />
     </main>
   );
 };
