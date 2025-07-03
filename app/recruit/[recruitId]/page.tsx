ef7bc470981e01d20c0f99251f671c2981b6b718
m01310g <m01310g@gmail.com>
2025-07-03 11:53:45 +0900

    [feat] 07/03 공고 상세 페이지 사이트 바로가기 버튼 구현 #53

--- app/recruit/[recruitId]/page.tsx
+++ app/recruit/[recruitId]/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import CenterNameSection from "./components/CenterNameSection";
 import TabTitle from "./components/TabTitle";
 import RecruitmentInfo from "./components/RecruitmentInfo";
+import RecruitFooter from "./components/RecruitFooter";
 
 const RecruitByIdPage = () => {
   const tabs = [
@@ -11,16 +12,24 @@ const RecruitByIdPage = () => {
     { key: "", label: "" },
   ];
   const [activeTab, setActiveTab] = useState("condition");
+  const site = "https://example.com";
 
   return (
-    <main className="h-[calc(100vh-46px)] overflow-y-auto bg-background-primary">
-      <CenterNameSection
-        centerName="00휘트니스 서현점"
-        createdAt="2025.11.03"
-      />
-      <TabTitle tabs={tabs} activeTab={activeTab} onTabChange={setActiveTab} />
-      {activeTab === "condition" ? <RecruitmentInfo /> : <></>}
-    </main>
+    <>
+      <main className="h-[calc(100vh-46px)] overflow-y-auto bg-background-primary scrollbar-none">
+        <CenterNameSection
+          centerName="00휘트니스 서현점"
+          createdAt="2025.11.03"
+        />
+        <TabTitle
+          tabs={tabs}
+          activeTab={activeTab}
+          onTabChange={setActiveTab}
+        />
+        {activeTab === "condition" ? <RecruitmentInfo /> : <></>}
+      </main>
+      <RecruitFooter site={site} />
+    </>
   );
 };
 
