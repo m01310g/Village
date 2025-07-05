0bfb5ae156f9bd72d133e79569e3758ad584c9a7
m01310g <m01310g@gmail.com>
2025-07-05 15:04:47 +0900

    [feat] 07/05 지역 필터링 디자인 수정

--- app/recruit/components/RegionFilterSettingBar.tsx
+++ app/recruit/components/RegionFilterSettingBar.tsx
@@ -5,7 +5,7 @@ const RegionFilterSettingBar = () => {
   const router = useRouter();
 
   return (
-    <div className="flex justify-between bg-background-brandSecondary px-4 py-3">
+    <div className="flex items-center justify-between bg-background-brandSecondary px-4 py-3">
       <span className="text-body-2 text-text-onsecondary">
         지역 조건을 설정해보세요.
       </span>
