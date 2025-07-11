81083c83888c75203b206997074bd6080612740d
m01310g <m01310g@gmail.com>
2025-07-11 10:12:03 +0900

    [feat] 07/11 무한 스크롤 적용 #100

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,31 +1,35 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useCallback, useState } from "react";
 import { useSetHeader } from "../components/header/HeaderContext";
 import FilteringButton from "../components/feed/FilteringButton";
 import PostCard from "../components/post/PostCard";
 import FloatingButton from "../components/post/FloatingButton";
 import { usePostList } from "./hooks/usePostList";
 import { useScrollRestoration } from "../lib/hooks/useScrollRestoration";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import { useScrollStore } from "@/store/useScrollStore";
 
 const Page = () => {
   const scrollRef = useRef<HTMLDivElement>(null);
+  const loaderRef = useRef<HTMLDivElement | null>(null);
   const { getActiveFilter, setActiveFilter: saveActiveFilter } =
     useScrollStore();
   const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const page = Number(searchParams.get("page") ?? 1);
   const [activeFilter, setActiveFilter] = useState(
     () => getActiveFilter(pathname) || "전체",
   );
-  const searchParams = useSearchParams();
-  const page = Number(searchParams.get("page") ?? 1);
+
+  const { data: postList } = usePostList(page);
 
   useScrollRestoration(scrollRef, activeFilter);
 
   const setHeader = useSetHeader();
   const filters = ["전체", "업계이야기", "채용", "교육"];
-  const { data: postList } = usePostList(page);
 
   useEffect(() => {
     setHeader({
@@ -49,6 +53,28 @@ const Page = () => {
           const typeIndex = filters.indexOf(activeFilter) - 1;
           return post.type === typeIndex;
         });
+
+  const loadNextPage = useCallback(() => {
+    const nextPage = page + 1;
+    router.push(`?page=${nextPage}`, { scroll: false });
+  }, [page, router]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const first = entries[0];
+        if (first.isIntersecting) {
+          loadNextPage();
+        }
+      },
+      { threshold: 1 },
+    );
+    const current = loaderRef.current;
+    if (current) observer.observe(current);
+    return () => {
+      if (current) observer.unobserve(current);
+    };
+  }, [loadNextPage]);
 
   return (
     <div className="flex h-full max-w-[375px] flex-col">
@@ -76,6 +102,7 @@ const Page = () => {
               />
             );
           })}
+        <div ref={loaderRef} className="h-2" />
       </div>
       <div className="fixed bottom-[81px] left-1/2 z-40 flex w-full max-w-[375px] -translate-x-1/2 px-4 pb-4">
         <FloatingButton />
